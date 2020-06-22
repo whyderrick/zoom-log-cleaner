c8239f84fc98f6c8c8d8cbc5ee88a66f9da0db06
Derrick Carr <whyderrick@gmail.com>
2020-06-21 21:57:29 -0700

    Add a second button to remove only timstamps
    This commit introduces a second button to the page that remove the
    timestamps but leaves names present.
    
    We're introducing a data attribute `remove-names` and using that
    attribute in the handler to detemine what to filter.

--- main.js
+++ main.js
@@ -1,14 +1,20 @@
 document.addEventListener("DOMContentLoaded", function(){
-  let button = document.querySelector("button");
-
-  button.onclick = function(event) {
-    const NAME_AND_TIMESTAMP = /^.+ : /;
-    let input = document.querySelector('[data-role="input"]');
-    let output = document.querySelector('[data-role="output"]');
-    let inputLines = input.value.split("\n");
-    let outputLines = inputLines.map((line) => line.replace(NAME_AND_TIMESTAMP, ""));
-
-    output.value = outputLines.join("\n");
-  }
+  let buttons = document.querySelectorAll("button");
+  buttons.forEach((button) => button.onclick = updateText)
 });
+
+function updateText() {
+  const NAME_AND_TIMESTAMP = /^.+ : /;
+  const TIMESTAMP = /^\S+\s+From / ;
+
+  const input = document.querySelector('[data-role="input"]');
+  const output = document.querySelector('[data-role="output"]');
+
+  const removalPattern = !!this.dataset.removeNames ? NAME_AND_TIMESTAMP : TIMESTAMP
+  const inputLines = input.value.split("\n");
+  const outputLines = inputLines.map((line) => line.replace(removalPattern, ""));
+
+  output.value = outputLines.join("\n");
+}
+
 
