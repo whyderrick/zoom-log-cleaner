c23ac241452f5d5fcdee8152cf65c8f8977d5c22
Derrick Carr <whyderrick@gmail.com>
2021-03-11 20:13:05 -0800

    Add the ability to filter for links
    Some conversations are full of links. This commit adds a new button to
    grab any message that contains "http". It also cleans some formatting
    around the textarea and does some wholly unnecesarry refactoring.

--- main.js
+++ main.js
@@ -1,20 +1,37 @@
 document.addEventListener("DOMContentLoaded", function(){
-  let buttons = document.querySelectorAll("button");
-  buttons.forEach((button) => button.onclick = updateText)
+  let cleanUpButtons = document.querySelectorAll("[name='clean']");
+  cleanUpButtons.forEach((button) => button.onclick = updateText)
+
+  let filterButtons = document.querySelectorAll("[name='filter']");
+  filterButtons.forEach((button) => button.onclick = filterLogs)
 });
 
-function updateText() {
-  const NAME_AND_TIMESTAMP = /^.+ : /;
-  const TIMESTAMP = /^\S+\s+From / ;
 
+const NAME_AND_TIMESTAMP = /^.+ : /;
+const TIMESTAMP = /^\S+\s+From / ;
+const URL = /http/;
+const PUBLIC_MESSAGE = / to Everyone :/;
+
+
+function updateText() {
   const input = document.querySelector('[data-role="input"]');
   const output = document.querySelector('[data-role="output"]');
 
   const removalPattern = !!this.dataset.removeNames ? NAME_AND_TIMESTAMP : TIMESTAMP
-  const inputLines = input.value.split("\n");
-  const outputLines = inputLines.map((line) => line.replace(removalPattern, ""));
+  let inputLines = input.value.split("\n");
+  let outputLines = inputLines.map((line) => line.replace(removalPattern, "").replace(PUBLIC_MESSAGE, ":"));
 
   output.value = outputLines.join("\n");
 }
 
+function filterLogs(matchingPattern = URL) {
+  const input = document.querySelector('[data-role="input"]');
+  const output = document.querySelector('[data-role="output"]');
 
+  let inputLines = input.value.split("\n");
+  let outputLines = inputLines
+      .filter((line) => line.match(/http/))
+      .map((line) => line.replace(TIMESTAMP, "").replace(PUBLIC_MESSAGE, ":"));
+
+  output.value = outputLines.join("\n");
+}
